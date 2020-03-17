2fde08441a9d80d9273dc05958ba4058221e59f9
leoncii <leonardotobe93@gmail.com>
2020-03-16 20:09:02 -0400

    -----test

--- src/components/Logo/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-
-export const Svg = styled.svg`
-    position: fixed;
-    height: 70px;
-    
-`
--- src/components/principal/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-
-export const Div = styled.div`
-    width: 100%;
-    margin: 0 auto;
-    height: 100%;
-`
